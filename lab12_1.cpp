7edb5b79562809b536a6902cd86d7d53f4e2c23f
nopphanat01 <sspas401@gmail.com>
2024-12-27 14:25:19 +0700

    ปป

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
+#include <ctime>
 using namespace std;
 
 int fibonacci(int x);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
+      clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start )/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
     return 0; 
 }
 
--- lab12_3.cpp
+++ lab12_3.cpp
@@ -2,16 +2,20 @@
 
 using namespace std;
 
-void mySwap(int &,int &); 
+void mySwap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
 
-int main(){
-	int x,y;
-	cin >> x >> y;
-	cout << "Before swapping:\n";
-	cout << "x = " << x << ", y = " << y << "\n";
-	mySwap(x,y);
-	cout << "After swapping:\n";
-	cout << "x = " << x << ", y = " << y << "\n";
-	
-	return 0;
+int main() {
+    int x, y;
+    cin >> x >> y;
+    cout << "Before swapping:\n";
+    cout << "x = " << x << ", y = " << y << "\n";
+    mySwap(x, y);
+    cout << "After swapping:\n";
+    cout << "x = " << x << ", y = " << y << "\n";
+    
+    return 0;
 }
